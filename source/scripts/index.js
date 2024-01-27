79402ba6eedc3bf57c0a447efff971137b172e79
Oleh Parkhomenko <oleh.parkhom@gmail.com>
2024-01-27 14:09:31 -0700

    Добавляет скрипт открытия и закрытия меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav__toggle');
+
+const recoling = function () {
+  if (nav.classList.contains('nav--closed')) {
+    nav.classList.remove('nav--closed');
+    nav.classList.add('nav--opened');
+  } else {
+    nav.classList.add('nav--closed');
+    nav.classList.remove('nav--opened');
+  }
+};
+
+nav.classList.remove('nav--nojs');
+
+navToggle.addEventListener('click', recoling);
